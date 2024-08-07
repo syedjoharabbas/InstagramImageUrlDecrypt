a160fc896c765a07ab06029483e0f785f7671b20
Johar Abbas <59311106+syedjoharabbas@users.noreply.github.com>
2024-08-07 08:42:22 +0400

    Commit

---
+++ InstagramImageDecrypt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Web;
+
+namespace NawafezBlazor
+{
+    public static class InstagramImageDecrypt
+    {
+        public static string Decrypt(string url)
+        {
+            var p = url.Split('/');
+            var t = new StringBuilder();
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (i == 2)
+                {
+                    t.Append(p[i].Replace("-", "--").Replace(".", "-"))
+                     .Append(Encoding.UTF8.GetString(Convert.FromBase64String("LnRyYW5zbGF0ZS5nb29n")))
+                     .Append('/');
+                }
+                else
+                {
+                    if (i != p.Length - 1)
+                    {
+                        t.Append(p[i]).Append('/');
+                    }
+                    else
+                    {
+                        t.Append(p[i]);
+                    }
+                }
+            }
+
+            var encodedUrl = Uri.EscapeUriString(t.ToString());
+            return encodedUrl;
+        }
+    }
+}
